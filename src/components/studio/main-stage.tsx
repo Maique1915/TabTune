a0d22798a64fc8077527f26ec8ac2a96cf461691
maique <maique1915@gmail.com>
2026-01-01 15:01:42 -0300

    refactor: Remove StageContainer wrapper from MainStage component.

--- src/components/studio/main-stage.tsx
+++ src/components/studio/main-stage.tsx
@@ -1,7 +1,7 @@
 import { useAppContext } from "@/app/context/app--context";
 import { VideoCanvasStage, type VideoCanvasStageRef } from "./video-canvas-stage";
 import React, { forwardRef } from 'react';
-import { StageContainer } from "@/components/shared/StageContainer";
+
 
 interface MainStageProps {
   onAnimationStateChange: (isAnimating: boolean, isPaused: boolean) => void;
@@ -17,17 +17,15 @@ export const MainStage = forwardRef<VideoCanvasStageRef, MainStageProps>(({ onAn
   } = useAppContext();
 
   return (
-    <StageContainer title="Studio Canvas">
-      <VideoCanvasStage
-        ref={ref}
-        chords={selectedChords}
-        timelineState={timelineState}
-        transitionsEnabled={playbackTransitionsEnabled}
-        buildEnabled={playbackBuildEnabled}
-        onAnimationStateChange={onAnimationStateChange}
-        onRenderProgress={setRenderProgress}
-      />
-    </StageContainer>
+    <VideoCanvasStage
+      ref={ref}
+      chords={selectedChords}
+      timelineState={timelineState}
+      transitionsEnabled={playbackTransitionsEnabled}
+      buildEnabled={playbackBuildEnabled}
+      onAnimationStateChange={onAnimationStateChange}
+      onRenderProgress={setRenderProgress}
+    />
   );
 });
 
